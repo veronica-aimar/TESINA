5ba778ec99e561b2866fbdea6787d1b4d1b7eda4
Veronica <aimarveronica1@gmail.com>
2021-05-01 15:16:57 +0200

    Prima fase creazione HomePage

--- SITO_WEB/home.php
+++ SITO_WEB/home.php
@@ -6,13 +6,40 @@
 <!doctype html>
 <html lang="it">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="../SRC/CSS/style.css">
         <title>HOME</title>
     </head>
     <body>
-        
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+        <nav class="navbar">
+            <div class="inner-width">
+                <a href="/" class="logo"></a>
+                <button class="menu-toggler"></button>
+                <div class="navbar-menu">
+                    <a href="#">HOME</a>
+                    <a href="#">CHI SIAMO</a>
+                    <a href="#">MEDICINALI</a>
+                    <a href="#">COMPRESSE</a>
+                    <a href="#">ASSORBENTI</a>
+                    <a href="#">ACCEDI</a>
+            </div>
+            </div>
+        </nav>
+
+        <div class="b1">
+            <div class="in1">
+                <div class="textBox">
+					<h2>TITLE</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+					<a href="#">Learn more</a>
+				</div>
+            </div>
+        </div>
     </body>
 </html>
